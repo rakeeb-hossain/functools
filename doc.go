0b3bf98a4f886cde4a6c59a4701377706098eb0c
rakeeb-hossain <rakeeb.hossain1@gmail.com>
2021-12-16 16:57:18 -0500

    Flatten module structure and add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+//Package functools includes a host of functional helpers using Go 1.18 generics.
+//
+//functools is intended to allow writing concise code without sacrificing performance or type-safety,
+//as is the case for functional libraries pre-generics.
+
+package functools
