d42e69f931b0fa935790354deb04558004062691
Rakeeb Hossain <rakeeb.hossain1@gmail.com>
2021-12-18 22:12:20 -0500

    Type approximation of slices in `chunk` (#7)

--- filter.go
+++ filter.go
@@ -8,8 +8,8 @@ package functools
 // predicate should be error-safe. It should handle any errors internally and return only a bool.
 // If other arguments are required by predicate, predicate should be made a closure with the appropriate
 // variables referenced.
-func Filter[T any, A ~[]T](slice A, predicate func(T) bool) []T {
-	res := make([]T, 0, len(slice))
+func Filter[T any, A ~[]T](slice A, predicate func(T) bool) A {
+	res := make(A, 0, len(slice))
 	for _, v := range slice {
 		if predicate(v) {
 			res = append(res, v)
