e11db97bd18a93b535203317684b4de3f21801f0
rakeeb-hossain <rakeeb.hossain1@gmail.com>
2021-12-18 22:11:16 -0500

    Make  use type approximation of slices

--- chunk.go
+++ chunk.go
@@ -10,26 +10,26 @@ import (
 // Vacuously, empty slices return an empty slice of slices regardless of the chunk size.
 //
 // When the chunk size is zero or negative, return an empty slice
-func Chunk[T any](slice []T, size int) [][]T {
+func Chunk[T any, A ~[]T](slice A, size int) []A {
 	n := len(slice)
 	if size <= 0 || n == 0 {
-		return [][]T{}
+		return []A{}
 	}
 
 	if size >= n {
-		tmp := make([]T, n)
+		tmp := make(A, n)
 		copy(tmp, slice)
-		return [][]T{tmp}
+		return []A{tmp}
 	}
 
-	res := make([][]T, 0, int(math.Ceil(float64(n/size))))
+	res := make([]A, 0, int(math.Ceil(float64(n/size))))
 	for i := 0; i < n; i += size {
 		if i+size <= n {
-			tmp := make([]T, size)
+			tmp := make(A, size)
 			copy(tmp, slice[i:i+size])
 			res = append(res, tmp)
 		} else {
-			tmp := make([]T, n-i)
+			tmp := make(A, n-i)
 			copy(tmp, slice[i:])
 			res = append(res, tmp)
 		}
